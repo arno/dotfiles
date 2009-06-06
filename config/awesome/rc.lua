09157dff7414fc0c785148512dae87a1bd3537bb
Arnaud Guignard <aguignard@gmail.com>
2009-06-06 18:16:01 +0200

    modify keybinding to go to previous/next tag

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -216,8 +216,8 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+    awful.key({ modkey, "Control" }, "Left",   awful.tag.viewprev       ),
+    awful.key({ modkey, "Control" }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
