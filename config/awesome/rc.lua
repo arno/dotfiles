7179beb88ebc6587aeda18c5369b8c3a05982648
Arnaud Guignard <aguignard@gmail.com>
2009-07-11 12:01:28 +0200

    Tagged Firefox 3.5 (aka Shiretoko)

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -73,6 +73,7 @@ floatapps =
 apptags =
 {
     ["Firefox"] = { screen = 1, tag = 2 },
+    ["Shiretoko"] = { screen = 1, tag = 2 },
     ["pidgin"] = { screen = 1, tag = 3 },
     ["Update-manager"] = { screen = 1, tag = 4 },
     ["Synaptic"] = { screen = 1, tag = 4 },
