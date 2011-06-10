22f14203cafadbad1c6fbbcbd561d0fda175fe8b
Arnaud Guignard <aguignard@gmail.com>
2011-06-10 20:27:01 +0200

    [awesome] comment unused workspace layouts

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -49,11 +49,11 @@ layouts =
 --    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
 --    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
+--    awful.layout.suit.spiral,
 --    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max.fullscreen,
+--    awful.layout.suit.max.fullscreen,
 --    awful.layout.suit.magnifier,
---    awful.layout.suit.floating
+    awful.layout.suit.floating
 }
 -- }}}
 
