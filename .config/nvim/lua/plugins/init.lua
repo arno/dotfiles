5472ac9492019c9d65bcd0af81b145fed0cef2f1
Arnaud Guignard <aguignard@gmail.com>
2025-12-29 12:00:06 +0000

    neovim: replace leap by flash

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -17,12 +17,17 @@ return {
 
   -- Jump everywhere
   {
-    "ggandor/leap.nvim",
-    config = function()
-      local bufopts = { silent=true }
-      vim.keymap.set("n", "<Space>", "<Plug>(leap-forward)", bufopts)
-      vim.keymap.set("n", "<Leader><Space>", "<Plug>(leap-backward)", bufopts)
-    end
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
 
   {
