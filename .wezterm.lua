ff05279125d26c717598a0007394f225f7caff5e
Arnaud Guignard <aguignard@gmail.com>
2026-01-27 18:52:32 +0000

    wezterm: use Catppuccin Latte theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,7 +13,8 @@ end
 -- This is where you actually apply your config choices
 
 -- Color scheme
-config.color_scheme = 'Terminal Basic'
+-- config.color_scheme = 'Terminal Basic'
+config.color_scheme = 'Catppuccin Latte'
 
 -- Tab bar
 config.hide_tab_bar_if_only_one_tab = true
